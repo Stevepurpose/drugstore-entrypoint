8ad9dddb7bfa22c50579f92d37d78ec1b2276a96
Stevepurpose <stephenodogwu83@gmail.com>
2023-03-28 01:34:37 +0100

    try feature

--- serverside/userController.js
+++ serverside/userController.js
@@ -15,7 +15,7 @@ try{
   let user=await User.login(email,password)
   let token=createToken(user._id)
   res.status(200).json({email,token})
-  console.log(user)
+  
 }
 catch(error){
 res.status(400).json({error:error.message})
